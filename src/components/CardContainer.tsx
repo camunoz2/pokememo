280874ef147e5d5b997b7a82a1c797e18b02b285
Cristian M <camunoz2@gmail.com>
2023-10-15 16:05:07 -0300

    rewrite logic

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,14 +1,10 @@
-import styles from "./card-container.module.css";
-
 interface Props {
-  children: JSX.Element[] | string | JSX.Element;
+  children: React.ReactNode;
 }
 
 const CardContainer = ({ children }: Props) => {
   return (
-    <div
-      className={`flex flex-wrap justify-around rounded-md gap-4 p-4 max-h-[700px] w-full overflow-scroll overflow-x-hidden ${styles.customscroll} ${styles.container}`}
-    >
+    <div className="flex flex-wrap justify-around rounded-md gap-4 p-4 max-h-[700px] w-full overflow-scroll overflow-x-hidden customscroll">
       {children}
     </div>
   );
--- src/components/DifficultyPicker.tsx
+++ src/components/DifficultyPicker.tsx
@@ -1,29 +1,8 @@
-interface Props {
-  level: number;
-  handleDifficulty: (difficultyLevel: number) => void;
-  difficulty?: number;
-  image: string;
-  text: string;
-}
-
-export function DiffilcultyPicker({
-  difficulty,
-  handleDifficulty,
-  level,
-  image,
-  text,
-}: Props) {
+export function DiffilcultyPicker() {
   return (
-    <div
-      onClick={() => handleDifficulty(level)}
-      className={`bg-color-lightblue flex flex-col p-1 rounded-md items-center cursor-pointer transition-all ${
-        difficulty === level
-          ? "border border-color-cyan shadow-color-cyan shadow-md "
-          : ""
-      }`}
-    >
-      <img src={image} className="aspect-square" alt="charmander" />
-      <p className="text-xl text-color-cyan">{text} </p>
+    <div className="bg-color-lightblue flex flex-col p-1 rounded-md items-center cursor-pointer transition-all">
+      <img src="/c1.png" className="aspect-square" alt="charmander" />
+      <p className="text-xl text-color-cyan">Easy </p>
     </div>
   );
 }
